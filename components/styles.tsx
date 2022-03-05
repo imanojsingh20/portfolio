b70a83c0fd027a33cd01003f64a234a55b5d3f9f
Manoj Singh <ext.manoj.s@goodworker.in>
2022-03-05 10:00:44 +0530

    initial commit

--- components/styles.tsx
+++ components/styles.tsx
@@ -14,15 +14,67 @@ export const StickyWrapper = styled.div`
 `;
 
 export const Container = styled.div`
-    display: flex;
     max-width: 1100px;
     margin: auto;
+    padding: 0 1rem;
+`;
+
+export const ShowCase = styled.div`
+    display: flex;
     flex-wrap: wrap;
     gap: 5rem;
-    padding: 0 1rem;
 
     @media only screen and (max-width: 768px) {
         gap: 1rem;
         flex-direction: column;
+    }
+`;
+
+export const Notes = styled.div`
+    margin: 1rem 0;
+    padding: 1rem;
+    width: 100%;
+    height: 90vh;
+    border-top: 1px solid ${({ theme }) => theme.colors.backgroundSecondary};
+    border-bottom: 1px solid ${({ theme }) => theme.colors.backgroundSecondary};
+
+    h2 {
+        text-align: center;
+    }
+`;
+
+export const NotesContentWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+    overflow-x: auto;
+    overflow-y: hidden;
+`;
+
+export const Input = styled.input<{ top: number; left: number }>`
+    position: absolute;
+    top: ${({ top }) => top + 'px'};
+    left: ${({ left }) => left + 'px'};
+    border: none;
+    outline: none;
+    padding: 0.5rem 1rem;
+    background: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.textPrimary};
+    font-size: 1.5rem;
+    z-index: 10;
+`;
+
+export const Label = styled.div<{ top: number; left: number; color: string; rotate: string }>`
+    max-width: 300px;
+    position: absolute;
+    top: ${({ top }) => top + 'px'};
+    left: ${({ left }) => left + 'px'};
+    background: ${({ color }) => color};
+    padding: 1rem;
+    border-radius: 5px;
+    transform: ${({ rotate }) => `rotate(${rotate})`};
+
+    @media only screen and (max-width: 768px) {
+        font-size: 0.8rem;
     }
 `;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,188 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useEffect, useRef, useState } from 'react';
 import AboutMe from '../components/AboutMe';
 import RightScrolableSection from '../components/RightScrollableSection';
-import { Container, StickyWrapper } from '../components/styles';
+import { Container, Input, Label, Notes, NotesContentWrapper, ShowCase, StickyWrapper } from '../components/styles';
+
+const data = [
+    {
+        top: 100,
+        left: 500,
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, ex?',
+        color: '#80ED99',
+        rotate: '10deg',
+    },
+    {
+        top: 500,
+        left: 300,
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam magni ut perspiciatis soluta a. Modi.',
+        color: '#00A19D',
+        rotate: '-15deg',
+    },
+];
+
+const colorArray = [
+    '#FF6633',
+    '#FFB399',
+    '#FF33FF',
+    '#FFFF99',
+    '#00B3E6',
+    '#E6B333',
+    '#3366E6',
+    '#999966',
+    '#99FF99',
+    '#B34D4D',
+    '#80B300',
+    '#809900',
+    '#E6B3B3',
+    '#6680B3',
+    '#66991A',
+    '#FF99E6',
+    '#CCFF1A',
+    '#FF1A66',
+    '#E6331A',
+    '#33FFCC',
+    '#66994D',
+    '#B366CC',
+    '#4D8000',
+    '#B33300',
+    '#CC80CC',
+    '#66664D',
+    '#991AFF',
+    '#E666FF',
+    '#4DB3FF',
+    '#1AB399',
+    '#E666B3',
+    '#33991A',
+    '#CC9999',
+    '#B3B31A',
+    '#00E680',
+    '#4D8066',
+    '#809980',
+    '#E6FF80',
+    '#1AFF33',
+    '#999933',
+    '#FF3380',
+    '#CCCC00',
+    '#66E64D',
+    '#4D80CC',
+    '#9900B3',
+    '#E64D66',
+    '#4DB380',
+    '#FF4D4D',
+    '#99E6E6',
+    '#6666FF',
+];
+
+const maxRotate = 45;
 
 const Home: NextPage = () => {
+    const [inputDetails, setInputDetails] = useState<{
+        top: number;
+        left: number;
+    } | null>();
+
+    const [labels, setLabels] = useState(data);
+
+    const InputRef = useRef<HTMLInputElement | null>(null);
+
+    const handleClick = (e: any) => {
+        if (e.target.id === 'notes-wrapper') {
+            setInputDetails(null);
+
+            const rect = e.target.getBoundingClientRect();
+            const x = e.clientX - rect.left; //x position within the element.
+            const y = e.clientY - rect.top; //y position within the element.
+            console.log('Left? : ' + x + ' ; Top? : ' + y + '.');
+
+            setInputDetails({
+                top: y,
+                left: x,
+            });
+        }
+    };
+
+    useEffect(() => {
+        InputRef.current?.select();
+    }, [inputDetails]);
+
+    const [innerWidth, setInnerWidth] = useState(0);
+
+    function reportWindowSize() {
+        console.log(window.innerWidth);
+        setInnerWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            window.onresize = reportWindowSize;
+
+            reportWindowSize();
+        }
+    }, []);
+
+    const handleSubmit = (e: any) => {
+        if (e.key === 'Enter') {
+            const random = Math.floor(Math.random() * (colorArray.length + 1));
+
+            const rotateDirection = Math.random() < 0.5 ? '-' : '';
+
+            const rotateDeg = Math.floor(Math.random() * (maxRotate + 1));
+
+            console.log(`${rotateDirection}${rotateDeg}deg`);
+
+            setLabels((prevState) => {
+                return [
+                    ...prevState,
+                    {
+                        top: inputDetails!.top,
+                        left: inputDetails!.left,
+                        text: e.target.value,
+                        color: colorArray[random],
+                        rotate: `${rotateDirection}${rotateDeg}deg`,
+                    },
+                ];
+            });
+
+            setInputDetails(null);
+        }
+    };
+
     return (
         <>
             <Head>
                 <title>Manoj Singh | Portfolio</title>
             </Head>
             <Container>
-                <StickyWrapper>
-                    <AboutMe />
-                </StickyWrapper>
-                <RightScrolableSection />
+                <ShowCase>
+                    <StickyWrapper>
+                        <AboutMe />
+                    </StickyWrapper>
+                    <RightScrolableSection />
+                </ShowCase>
+                <Notes>
+                    <small>Just click and start typing...</small>
+                    <NotesContentWrapper id='notes-wrapper' onClick={handleClick}>
+                        {inputDetails && (
+                            <Input
+                                top={inputDetails.top}
+                                left={inputDetails.left}
+                                defaultValue='Start typing here!'
+                                autoFocus
+                                ref={InputRef}
+                                onKeyDown={handleSubmit}
+                            />
+                        )}
+                        {labels.map(({ top, left, color, text, rotate }, index) => {
+                            return (
+                                <Label key={index} top={top} left={left} color={color} rotate={rotate}>
+                                    {text}
+                                </Label>
+                            );
+                        })}
+                    </NotesContentWrapper>
+                </Notes>
             </Container>
         </>
     );
