1302f6d7a2360b4752ec86f01d6561b004014117
Manoj Singh <ext.manoj.s@goodworker.in>
2022-05-06 05:10:19 +0530

    added corte project

--- components/Projects/constant.ts
+++ components/Projects/constant.ts
@@ -1,6 +1,6 @@
 const data = [
     {
-        tag: ['ReactJs'],
+        tag: ['TypeScript', 'ReactJs', 'GraphQL'],
         title: 'GoodWorker',
         desc: 'PWA, focus on empowering blue collar jobs in India',
         link: {
@@ -9,16 +9,16 @@ const data = [
         },
     },
     {
-        tag: ['NextJS', 'Express'],
-        title: 'Maido',
-        desc: 'Multi-user blogging platform front and back, where users can signup and post articles.',
+        tag: ['ReactJs', 'Express', 'MongoDB'],
+        title: 'Corte | Url Shortener',
+        desc: 'Corte means cut in spanish, this is how Corte came up. ',
         link: {
             external: true,
-            url: 'https://article-nextjs.vercel.app',
+            url: 'https://corte.manojsingh.tech/',
         },
     },
     {
-        tag: ['ReactJs', 'Express'],
+        tag: ['ReactJs', 'Express', 'PostgreSQL'],
         title: 'MakeMyTrip Clone',
         desc: 'Full functional MMT flights clone front and back, with payment integration and flight seat selection.',
         link: {
