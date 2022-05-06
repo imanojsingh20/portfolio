1a1709f3e73a6d91cb276774d06dfb98951ceec6
Manoj Singh <ext.manoj.s@goodworker.in>
2022-05-06 08:46:53 +0530

    add breadcrumbs

--- components/BreadCrumb/index.tsx
+++ components/BreadCrumb/index.tsx
@@ -1,43 +1,61 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { BreadCrumbStyled } from './styles';
 import Link from 'next/link';
 
 const BreadCrumb = () => {
+    const [collapsed, setCollapsed] = useState(true);
     const router = useRouter();
 
     const currentRoute = router.asPath;
     const crumbs = currentRoute.split('/');
 
+    const toggleCollapse = () => {
+        setCollapsed((prevState) => !prevState);
+    };
+
     const getHref = (path: string, crumbs: string[], position: number): string => {
         return crumbs.slice(0, position).join('/') || '/';
     };
 
-    return (
-        <BreadCrumbStyled>
-            {crumbs.map((path, index) => {
-                if (!path) {
-                    return (
-                        <Link href={getHref(path, crumbs, index + 1)} key={path + index}>
-                            <a>
-                                <Image src='/assets/logo.png' alt='Picture of the author' width={50} height={50} />
-                            </a>
-                        </Link>
-                    );
-                }
+    const breadCrumbElements = crumbs.map((path, index) => {
+        if (!path) {
+            return (
+                <li key={path + index}>
+                    <Link href={getHref(path, crumbs, index + 1)}>
+                        <a>
+                            <Image src='/assets/logo.png' alt='Picture of the author' width={50} height={50} />
+                        </a>
+                    </Link>
+                </li>
+            );
+        }
 
-                return (
-                    <React.Fragment key={path + index}>
-                        <span>&#60;</span>
-                        <Link href={getHref(path, crumbs, index + 1)}>
-                            <a>{path.replace('-', ' ')}</a>
-                        </Link>
-                    </React.Fragment>
-                );
-            })}
-        </BreadCrumbStyled>
+        return (
+            <React.Fragment key={path + index}>
+                <span>&#60;</span>
+                <li>
+                    <Link href={getHref(path, crumbs, index + 1)}>
+                        <a>{path.replace('-', ' ')}</a>
+                    </Link>
+                </li>
+            </React.Fragment>
+        );
+    });
+
+    const middleElement = (
+        <React.Fragment key='collapse'>
+            <span>&#60;</span>
+            <li className='collapsed'>
+                <span onClick={toggleCollapse}>...</span>
+            </li>
+        </React.Fragment>
     );
+
+    const collapedVersion = [breadCrumbElements[0], middleElement, breadCrumbElements[breadCrumbElements.length - 1]];
+
+    return <BreadCrumbStyled>{collapsed ? collapedVersion : breadCrumbElements}</BreadCrumbStyled>;
 };
 
 export default BreadCrumb;
--- components/BreadCrumb/styles.tsx
+++ components/BreadCrumb/styles.tsx
@@ -1,9 +1,31 @@
 import styled from 'styled-components';
 
-export const BreadCrumbStyled = styled.div`
+export const BreadCrumbStyled = styled.ul`
     display: flex;
     align-items: center;
-    gap: 3rem;
-
+    gap: 2rem;
     text-transform: capitalize;
+    list-style: none;
+    overflow: hidden;
+    flex-wrap: wrap;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+
+    a {
+        text-decoration: underline;
+    }
+
+    li:last-child {
+        width: 15rem;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+    }
+
+    .collapsed {
+        overflow: auto;
+
+        cursor: pointer;
+        text-decoration: underline;
+    }
 `;
