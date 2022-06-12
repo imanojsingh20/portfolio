11463eb12deeb65f7e9c97b349af6ea7644fc462
Manoj Singh <ext.manoj.s@goodworker.in>
2022-06-12 13:37:27 +0530

    test codeblock lag

--- components/ReactMarkDown/CodeBlock.tsx
+++ components/ReactMarkDown/CodeBlock.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import dracula from 'react-syntax-highlighter/dist/cjs/styles/prism/dracula';
 import { Components } from 'react-markdown';
 
 type Props = {
